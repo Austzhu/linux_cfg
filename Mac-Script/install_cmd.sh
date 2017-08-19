5f2ad2b416c0a8201ba396e78da8941718f70fd9
Austzhu <153462902@qq.com>
2017-08-19 18:03:41 +0800

    add some vim plugin

---
+++ Mac-Script/install_cmd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ln -sf ./run qq
+ln -sf ./run chrome
+ln -sf ./run calc
+ln -sf ./run facetime
+ln -sf ./run pdf
+ln -sf ./run qqlive
+ln -sf ./run music
+ln -sf ./run show
+ln -sf ./run word
+ln -sf ./run excel
+ln -sf ./run dic
+ln -sf ./run vpn
+ln -sf ./run lantern
+ln -sf ./run system
+ln -sf ./run terminal
+ln -sf ./run iTerm
+ln -sf ./run subl
+ln -sf ./run plist
+ln -sf ./run meld`
+
+exit 0
